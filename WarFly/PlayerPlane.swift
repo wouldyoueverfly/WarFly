242f88d6bedfe96b0bfebe84e92ac9bd894b9803
wouldyoueverfly <cpptypxe@ya.ru>
2025-06-27 21:25:06 +0300

    [Feature] Plane Movement (CoreMotion)

---
+++ WarFly/PlayerPlane.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerPlane.swift
+//  WarFly
+//
+//  Created by Ruslan Khusainov on 27.06.2025.
+//
+
+import SpriteKit
+
+class PlayerPlane: SKSpriteNode {
+    static func populate(at point: CGPoint) -> SKSpriteNode {
+        let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
+        let playerPlane = SKSpriteNode(texture: playerPlaneTexture)
+        playerPlane.setScale(0.5)
+        playerPlane.position = point
+        playerPlane.zPosition = 20
+        return playerPlane
+    }
+}
